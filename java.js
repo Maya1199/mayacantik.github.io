92258b42786e276d4f2d11d12d9bd5de869726cd
Maya1199 <70632955+Maya1199@users.noreply.github.com>
2020-12-14 06:46:49 +0700

    Create java.js

---
+++ java.js
@@ -0,0 +1,7 @@
+var n= prompt("SELAMAT DATANG DI BLOG MAYAVILLA");
+var c= confirm("haLLO<" +n+ ">Lanjutkan");
+if (c== true){
+    alert ('Happy enjoy di MayaVila');
+}else{
+    alert('Selamat Bersenang-senang');
+}
